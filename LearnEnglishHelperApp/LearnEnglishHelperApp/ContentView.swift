6122e0f028043b781c09428a1d5aabd0ea709566
老房东 <huangdong@gmail.com>
2022-02-22 17:28:41 -0500

    初始化新项目结构

---
+++ LearnEnglishHelperApp/LearnEnglishHelperApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnEnglishHelperApp
+//
+//  Created by 老房东 on 2022-02-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnEnglishHelperApp/LearnEnglishHelperApp/LearnEnglishHelperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnEnglishHelperAppApp.swift
+//  LearnEnglishHelperApp
+//
+//  Created by 老房东 on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct LearnEnglishHelperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
