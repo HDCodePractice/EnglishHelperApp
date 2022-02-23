69dd3ffb24992c7010efd6a991be0040af1c4275
老房东 <huangdong@gmail.com>
2022-02-23 18:50:20 -0500

    加入空白GridView

---
+++ WordSearch/Sources/WordSearch/GridView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by 老房东 on 2022-02-23.
+//
+
+import SwiftUI
+
+struct GridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridView()
+    }
+}
