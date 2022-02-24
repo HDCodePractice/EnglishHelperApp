12502b74a77479603a5ba622d745825b11830ada
sunnypo <46382526+sunnypo@users.noreply.github.com>
2022-02-23 21:11:49 -0500

    letter matrix formed in matrix

--- WordSearch/Sources/WordSearch/GridView.swift
+++ WordSearch/Sources/WordSearch/GridView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct GridView: View {
+    @State var selected : Bool = false
+    
+    var text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader{g in
+            ZStack {
+                Rectangle().fill(.gray).border(.white)
+                Text(text).font(.system(size: g.size.height > g.size.width ? g.size.width : g.size.height))
+                    .foregroundColor(selected ? .red : .primary)
+                    .onTapGesture {
+                    selected.toggle()
+                }
+            }
+        }
     }
 }
 
 struct GridView_Previews: PreviewProvider {
     static var previews: some View {
-        GridView()
+        GridView(text: "E")
     }
 }
