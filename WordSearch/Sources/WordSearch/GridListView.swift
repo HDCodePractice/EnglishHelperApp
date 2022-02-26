031848ec123d2291b4cf730da1dcf7422e343796
hendryshao <87082007+hendryshao@users.noreply.github.com>
2022-02-26 10:03:20 -0500

    写     不    出    来

--- WordSearch/Sources/WordSearch/GridListView.swift
+++ WordSearch/Sources/WordSearch/GridListView.swift
@@ -4,17 +4,38 @@
 //
 //  Created by 老房东 on 2022-02-23.
 //
-
 import SwiftUI
 
 struct GridListView: View {
+    @State var vm = ViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:0){
+            ForEach(vm.grid, id: \.self){
+                row in
+                HStack(spacing:0){
+                    
+                }
+            }
+        }
     }
 }
 
 struct GridListView_Previews: PreviewProvider {
     static var previews: some View {
         GridListView()
+        Group {
+            GridListView()
+            GridListView().previewInterfaceOrientation(.landscapeLeft)
+        }
     }
 }
+
+struct ViewModel{
+    let grid = [
+        ["q","w","e","r","t","y","u","i","o","p"],
+        ["a","s","d","f","g","h","j","k","l"],
+        ["z","x","c","v","b","n","m",",","."]
+
+    ]
+}
+print (grid)
