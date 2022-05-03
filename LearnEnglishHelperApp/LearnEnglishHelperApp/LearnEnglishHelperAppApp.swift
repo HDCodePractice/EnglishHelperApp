985f148348ff15e21a4790857d1bb58b286c56cf
老房东 <huangdong@gmail.com>
2022-05-03 08:14:31 -0400

    启动和恢复时进行数据同步

--- LearnEnglishHelperApp/LearnEnglishHelperApp/LearnEnglishHelperAppApp.swift
+++ LearnEnglishHelperApp/LearnEnglishHelperApp/LearnEnglishHelperAppApp.swift
@@ -9,9 +9,42 @@ import SwiftUI
 
 @main
 struct LearnEnglishHelperAppApp: App {
+    var vm : LearnEnglishHelperViewModel
+    @Environment(\.scenePhase) var scenePhase
+    
+    init(){
+        let v = LearnEnglishHelperViewModel()
+        vm = v
+        Task.init {
+            v.isLoading = true
+            await v.fetchData()
+            v.isLoading = false
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(vm)
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase{
+            case .active:
+                print("app active")
+                Task.init {
+                    if vm.isLoading == false{
+                        vm.isLoading = true
+                        await vm.fetchData()
+                        vm.isLoading = false
+                    }
+                }
+            case .inactive:
+                print("app inactive")
+            case .background:
+                print("app background")
+            default:
+                print("app unknow status")
+            }
         }
     }
 }
