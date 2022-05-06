1e98dc1624aff23df4e91709c7d57214907629bf
老房东 <huangdong@gmail.com>
2022-05-06 11:20:24 -0400

    支持在picturegame中双击放大显示图片，再单击恢复

--- PictureGame/Sources/PictureGame/ViewComponents/AnswerGrid.swift
+++ PictureGame/Sources/PictureGame/ViewComponents/AnswerGrid.swift
@@ -6,33 +6,66 @@
 //
 
 import SwiftUI
+import CommomLibrary
 
 struct AnswerGrid: View {
     @EnvironmentObject var vm : PictureGameViewModel
+    @State var isShowOne = false
+    @State var showOneAnswer: Answer?
     
     var body: some View {
-        VStack(spacing:10){
-            HStack(spacing:10){
-                ForEach(vm.answerChoices.prefix(3)){ item in
-                    AnswerRow(answer: item)
-                        .onTapGesture {
-                            if !vm.answerSelected{
-                                vm.selectAnswer(answer: item)
-                            }
-                        }
+        if let showOneAnswer=showOneAnswer,isShowOne{
+            ZStack{
+                Color.clear
+                PictureView(
+                    url: URL(string:showOneAnswer.picUrl),
+                    errorMsg: showOneAnswer.name
+                )
+                .shadow(color: showOneAnswer.isSelected ? (showOneAnswer.isCorrect ? .green : .red) : .gray, radius: 5, x: 0.5, y: 0.5)
+                .onTapGesture{
+                    withAnimation {
+                        isShowOne = false
+                    }
                 }
             }
-            HStack{
-                ForEach(vm.answerChoices.suffix(3)){ item in
-                    AnswerRow(answer: item)
-                        .onTapGesture {
-                            if !vm.answerSelected{
-                                vm.selectAnswer(answer: item)
+        }else{
+            VStack(spacing:10){
+                HStack(spacing:10){
+                    ForEach(vm.answerChoices.prefix(3)){ item in
+                        AnswerRow(answer: item)
+                            .onTapGesture(count: 2) {
+                                withAnimation {
+                                    isShowOne = true
+                                    showOneAnswer = item
+                                    
+                                }
                             }
-                        }
+                            .onTapGesture(count: 1) {
+                                if !vm.answerSelected{
+                                    vm.selectAnswer(answer: item)
+                                }
+                            }
+                    }
                 }
+                HStack{
+                    ForEach(vm.answerChoices.suffix(3)){ item in
+                        AnswerRow(answer: item)
+                            .onTapGesture(count: 2) {
+                                withAnimation {
+                                    isShowOne = true
+                                    showOneAnswer = item
+                                    
+                                }
+                            }
+                            .onTapGesture(count: 1) {
+                                if !vm.answerSelected{
+                                    vm.selectAnswer(answer: item)
+                                }
+                            }
+                    }
+                }
+                
             }
-
         }
     }
 }
