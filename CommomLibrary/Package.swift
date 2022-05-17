5da1a5a1cdb4d0e3cc630daafb1a575f4f946ec6
老房东 <huangdong@gmail.com>
2022-05-16 21:37:37 -0400

    将SyncKit变更回realm-swift

--- CommomLibrary/Package.swift
+++ CommomLibrary/Package.swift
@@ -17,8 +17,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.2.0"),
-        .package(url: "https://github.com/mentrena/SyncKit.git", from: "1.3.0")
-//        .package(url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.23.0"))
+        .package(url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.23.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,7 +26,7 @@ let package = Package(
             name: "CommomLibrary",
             dependencies: [
                 "Kingfisher",
-                .product(name: "SyncKit/RealmSwift", package: "SyncKit")
+                .product(name: "RealmSwift", package: "realm-swift")
             ],
             resources: [
                 .process("DBModel/example.json")
